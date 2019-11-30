8f75ac71947f74bb18cbd468ae13ee18f5dcfa7d
Golden Boy <goldenboy@Goldens-MacBook-Pro.local>
2019-11-30 15:45:25 +0700

    add remove function and some style

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //import staff
 import React from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 
 //create staff
 class App extends React.Component {
@@ -14,20 +14,28 @@ class App extends React.Component {
     arr.push(newTodo);
     this.setState({ todo: arr, text: "" });
   }
+  deleteTodo = (t) => {
+    var arr = this.state.todo;
+    var pos = arr.indexOf(t);
+    arr.splice(arr, 1);
+    this.setState({ todo: arr });
+  }
   renderTodos = () => {
     return this.state.todo.map(t => {
       return (
-        <Text
-          key={t}
-          onPress={this.deleteTodo}
-        >{t}</Text>
+        <TouchableOpacity key={t}>
+          <Text
+            style={styles.todo}
+            onPress={() => { this.deleteTodo(t) }}
+          >{t}</Text>
+        </TouchableOpacity>
       )
     })
   }
   render() {
     return (
       <View style={styles.viewStyle}>
-        <Text>Please Input Todo List Below</Text>
+        <Text style={styles.header}>Add Your ToDo List Below</Text>
         <TextInput
           style={styles.inputStyle}
           onChangeText={(text) => this.setState({ text })}
@@ -35,8 +43,10 @@ class App extends React.Component {
         />
         <Button
           title="Add Todo"
+          color="green"
           onPress={this.addTodo}
         />
+        <View style={{ marginTop: 30 }} />
         {this.renderTodos()}
       </View>
     )
@@ -45,15 +55,25 @@ class App extends React.Component {
 
 const styles = {
   viewStyle: {
-    flex: 1,
+    marginTop: 200,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    margin: 20,
   },
   inputStyle: {
     height: 30,
-    borderColor: 'black',
+    borderColor: 'green',
     borderWidth: 1,
-  }
+  },
+  header: {
+    fontSize: 25,
+    color: 'green',
+    fontWeight: 'bold',
+  },
+  todo: {
+    fontSize: 15,
+    color: 'black',
+  },
 }
 
 //export staff
